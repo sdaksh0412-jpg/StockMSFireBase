881e3b1d62c1717e27ce740f4074d4d5ddf81142
sdaksh0412-jpg <sdaksh0412@gmail.com>
2025-08-24 19:14:58 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+ const firebaseConfig = {
+    apiKey: "AIzaSyAR82dGiLHuvEln7yM1wOMt4c_Y5n4nuEg",
+    authDomain: "leadmanagementsystem-71fee.firebaseapp.com",
+    projectId: "leadmanagementsystem-71fee",
+    storageBucket: "leadmanagementsystem-71fee.firebasestorage.app",
+    messagingSenderId: "731798351726",
+    appId: "1:731798351726:web:0e0ec55a0f3e61b9ca9b7f",
+    measurementId: "G-D5NB243C9R"
+  };
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
